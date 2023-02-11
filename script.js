bc3cf361cdfa1fdb3045c87542084dfa23912121
hamdtc <mohamedtc73@gmail.com>
2023-02-11 19:30:09 +0530

    final

--- script.js
+++ script.js
@@ -8,20 +8,47 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+   arr.map(
+      function(arr){
+        if(arr.marks > 50){
+            console.log(arr);
+        }
+    });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(
+        function(array){
+          if(array.marks > 50 ){
+           console.log(array);
+          }
+      });
 }
 
 function addData() {
   //Write your code here, just console.log
+  var data={id:4,name:"susan",age:"20",marks:45};
+    arr.push(data);
+    console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  var newArr=arr.filter(removeStd);
+    function removeStd(arr){
+      return arr.marks >= 50;
+    }
+    console.log(newArr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+   var newArray= [
+      { id: 5, name: "tommy", age: "18", marks: 90 },
+      { id: 6, name: "arthur", age: "20", marks: 60 },
+      { id: 7, name: "polly", age: "26", marks: 85 },
+    ];
+    var finalArray= arr.concat(newArray);
+    console.log(finalArray);
 }
